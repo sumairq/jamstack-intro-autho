f3345a372240cd8f5aaa44d7ccbe49ef2e22aa8b
Sumair <sumairqaisar992@gmail.com>
2020-11-09 03:32:12 +0500

    feat(Oauth login): made it functional

--- src/components/route-login.js
+++ src/components/route-login.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import {useIdentityContext} from 'react-netlify-identity';
+import {navigate} from 'gatsby';
 
 
+const RouteLogin = ({ showModal }) => {
+ const identity = useIdentityContext();
 
-const RouteLogin = () => <>
+ if (identity && identity.isLoggedIn) {
+     navigate('/dashboard/secret', { replace: true });
+ }
+return (
+  <>
     <h1>Log In or Sign Up to this service</h1>
-    <button>Log In</button>
+    <button onClick={showModal}>Log In</button>
     </>
-
+);
+}
 
 
 export default RouteLogin;
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { navigate } from 'gatsby';
+import IdentityModal from 'react-netlify-identity-widget';
 import { Router } from '@reach/router'; 
 import Layout from '../components/layout';
 import Profile from '../components/profile';
@@ -7,13 +8,17 @@ import RouteBase from '../components/route-base'
 import RouteSecret from '../components/route-secret'
 import RouteLogin from '../components/route-login'
 
+import 'react-netlify-identity-widget/styles.css'
+
 const Dashboard = ({location}) => {
+const [isVisible, setVisibility] = useState(false);
 useEffect(() => {
     if(location.pathname.match(/^\/dashboard\/?$/)){
         navigate("/dashboard/login", {replace: true});
     }
 },[]);  
-
+ 
+const showModal = () => setVisibility(true); 
 
     return (
         <Layout>
@@ -21,8 +26,12 @@ useEffect(() => {
             <Router>
             <RouteBase path="/dashboard/base"/>
             <RouteSecret path="/dashboard/secret"/>
-            <RouteLogin path="/dashboard/login"/>
+            <RouteLogin path="/dashboard/login" 
+            showModal = {showModal}/>   
             </Router>
+            <IdentityModal
+              showDialog = {isVisible} onCloseDialog = {() => setVisibility(false)}
+            />
         </Layout>
     )
 }
